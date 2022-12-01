0d004093906de7f60b73f4679cb05fe790f3a789
chirsling <lingliang1100@gmail.com>
2022-12-01 12:08:23 +0800

    fix: 指标复制内容切换为metric_id (#17)

--- bkmonitor-timeseries-datasource/src/components/metirc-input.tsx
+++ bkmonitor-timeseries-datasource/src/components/metirc-input.tsx
@@ -509,7 +509,7 @@ export default class MonitorQueryEditor extends React.PureComponent<IQueryProps,
                   onMouseLeave={() => this.handleHideTool(metric)}>
                   <div className='metric-item-title'>
                     <span style={{ fontSize: 0, opacity: 0 }}
-                      id={metric.metric_id}>{MetricInputMode.COPY === this.props.mode ?  metric.readable_name.replace(/\./g, ':') : metric.readable_name}
+                      id={metric.metric_id}>{metric.metric_id.replace(/\./g, ':')}
                     </span>
                     <div className='title-wrap'>
                       <span className='title-name'>{this.getSearchNode(metric.readable_name)}</span>{this.getSearchNode(metric.titleAlias)}
