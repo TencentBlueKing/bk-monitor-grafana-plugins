c28528b9946a70e1864b62ff8ac736ae8d8aeac8
liangling <lingliang1100@gmail.com>
2024-03-06 10:53:15 +0800

    feat: 升级插件构建 # Reviewed, transaction id: 3388

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,8 @@
 module.exports = {
   arrowParens: 'always',
-  arrowParens: 'avoid',
   bracketSameLine: false,
   jsxSingleQuote: true,
   printWidth: 120,
-  quoteProps: 'as-needed',
   quoteProps: 'as-needed',
   semi: true,
   singleAttributePerLine: true,
