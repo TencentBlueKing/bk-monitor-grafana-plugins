5f10ee7155a5e3e5deca003b6048cbacd6559aa0
xiaolicheng-github <2933458919@qq.com>
2025-06-27 17:25:27 +0800

    feat: 【仪表盘】针对时序图，增加最后一个数据点的展示

--- src/timeseries/src/datasource/datasource.ts
+++ src/timeseries/src/datasource/datasource.ts
@@ -843,7 +843,7 @@ export default class DashboardDatasource extends DataSourceApi<QueryData, QueryO
                 data: {
                   display: config.display,
                   down_sample_range: item.enableDownSampling !== false ? down_sample_range : undefined,
-                  show_last_point: item.showLastPoint === true ? true : undefined,
+                  time_alignment: item.showLastPoint === true ? false : undefined,
                   end_time: options.range.to.unix(),
                   expression: config.refId || '',
                   format: item.format,
@@ -878,7 +878,7 @@ export default class DashboardDatasource extends DataSourceApi<QueryData, QueryO
           const params = {
             data: {
               down_sample_range: item.enableDownSampling === true ? down_sample_range : undefined, // promql 原来默认是不开启的
-              show_last_point: item.showLastPoint === true ? true : undefined,
+              time_alignment: item.showLastPoint === true ? false : undefined,
               end_time: options.range.to.unix(),
               format: item.format,
               promql: this.buildPromqlVariables(item.source!, {
@@ -918,7 +918,7 @@ export default class DashboardDatasource extends DataSourceApi<QueryData, QueryO
                 data: {
                   display: exp.active,
                   down_sample_range: enableDownSampling !== false ? down_sample_range : undefined,
-                  show_last_point: showLastPoint === true ? true : undefined,
+                  time_alignment: showLastPoint === true ? false : undefined,
                   end_time: options.range.to.unix(),
                   expression: exp.expression,
                   format: item.format,
